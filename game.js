4521ab8da42dc966fb17263db419e97beb7ef074
biswajitmaji <biswajitmaji.tech@gmail.com>
2014-12-11 01:51:32 +0530

    basic sequence checking logic added.

--- game.js
+++ game.js
@@ -1,15 +1,64 @@
 var ximg = "media/image1/orig.jpeg",
     oimg = "media/image2/orig.jpeg";
 
-var checkSequence = function(curXYPos, DiceType) {
+var checkEachSequence = function(eachSeqArr, DiceType) {
+    // if both in sequencearr has same with DiceType, then return true
+    // else false;
+    console.log(eachSeqArr);
+    return false;
+};
+var checkAllSequence = function(curXYPos, DiceType) {
     var posXYArr = curXYPos.split('-');
     // x axis wise loop to get other 2 td to create sequence
+    var eachSequenceArr, eachSequenceIndex = 0;
     for (i=1; i<=3;i++) {
         if (curXYPos == (posXYArr[0]+'-'+i)) { continue; }
+        
+        eachSequenceArr[eachSequenceIndex++] = posXYArr[0]+'-'+i;
+    }
+// call to check each sequence
+if(checkEachSequence(eachSequenceArr, DiceType)) { return true; }
+    
+    eachSequenceIndex = 0;
+    for (i=1; i<=3;i++) {
+        if (curXYPos == (i+'-'+posXYArr[0])) { continue; }
+        
+        eachSequenceArr[eachSequenceIndex++] = i+'-'+posXYArr[0];
+    }
+// call to check each sequence : if true -> return true
+if(checkEachSequence(eachSequenceArr, DiceType)) { return true; }
+    
+    // add checking for corner positions
+    switch (curXYPos) {
+        case '1-1':
+            eachSequenceArr = ["2-2", "3-3"];
+            break;
+        case '3-3':
+            eachSequenceArr = ["1-1", "2-2"];
+            break;
+        case '1-3':
+            eachSequenceArr = ["2-2", "3-1"];
+            break;
+        case '3-1':
+            eachSequenceArr = ["2-2", "1-3"];
+            break;
+    }
+// call to check each sequence
+if(checkEachSequence(eachSequenceArr, DiceType)) { return true; }
+
+    //for special position 2-2
+    if (curXYPos) {
+        eachSequenceArr = ["3-1", "1-3"];
+// call to check each sequence
+if(checkEachSequence(eachSequenceArr, DiceType)) { return true; }
+        eachSequenceArr = ["1-1", "3-3"];
+// call to check each sequence
+if(checkEachSequence(eachSequenceArr, DiceType)) { return true; }
     }
     
-    
+    return false;
 }
+
 
 var checkComplition = function() { //check all image position if any sequence completed
     //var currentSeries = '';
