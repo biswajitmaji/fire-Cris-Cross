e0443443dc6bea5b889b2ba40cfc5762393beb09
Biswajit Maji <biswajitm@mindfiresolutions.com>
2015-01-05 19:56:03 +0530

    basic rule added when comp using X.

--- game.js
+++ game.js
@@ -173,6 +173,107 @@ var compRunningZero = function() {
     }
 
 };
+var compRunningX = function() {
+    // determine first placement.
+    if (0 == $("td.cross").length) {
+        // place cross in random place among 4 corners and middle
+        // get x axis from 1,2,3
+        var y, x = Math.floor(3 * Math.random()) % 3 + 1;
+        if (2 == x) {
+            y = 2;
+        } else {
+            var myArray = [1, 3];
+            y = myArray[Math.floor(Math.random() * myArray.length)];
+        }
+        $("#"+x+"-"+y).removeClass("blank").addClass("cross");
+    } else { // 2nd turn onwards
+        var RuleFound = 0;
+
+        // attacking rule - loop through blanks and check if comp can win
+        $('#gameTable tr td.blank').each(function() {
+            if (checkAllSequence(this.id, 'cross')) {
+                $("#" + this.id).removeClass("blank").addClass("cross");
+                alert('Comp win');
+                RuleFound = 1;
+                return false;
+            }
+        });
+
+        // defending rule - loop through blanks and defend if user win possible
+        if (0 == RuleFound) {
+            $('#gameTable tr td.blank').each(function() {
+                if (checkAllSequence(this.id, 'zero')) {
+                    $("#" + this.id).removeClass("blank").addClass("cross");
+                    RuleFound = 1;
+                    return false;
+                }
+            });
+        }
+
+        // exceptional logic if above 2 dont work
+//        if (0 == RuleFound) {
+//            if($("#2-2").hasClass('zero')) { // zero in center
+//                // if corner and opposite is the position (determine this case)
+//                var Middle4Pos = new Object({"1-2":["3-1","3-3"], "2-1":["1-3","3-3"], "2-3":["1-1","3-1"], "3-2":["1-1","1-3"]}),
+//                        excludeList = [];
+//                $.each(Middle4Pos, function(key, val) {
+//                    if($("#"+key).hasClass('cross')) {
+//                        // find if any of opposite corner has values
+//                        $.each(val, function(i, eachCornerVal) {
+//                            if($("#"+eachCornerVal).hasClass('cross')) { // "2.5" combination found
+//                                var middlePosXYArr = key.split('-');
+//                                if(2 == middlePosXYArr[0]) {
+//                                    // if row of middle position is 2, dont touch the whole col of corner pos
+//                                    var cornerCol = eachCornerVal.split('-')[1];
+//                                    excludeList.push("1-"+cornerCol, "2-"+cornerCol, "3-"+cornerCol);
+//                                } else {
+//                                    // if col of middle position is 2, dont touch the whole row of corner pos
+//                                    var cornerRow = eachCornerVal.split('-')[0];
+//                                    excludeList.push(cornerRow+"-1", cornerRow+"-2", cornerRow+"-3");
+//                                }
+//                            }
+//                        });
+//                    }
+//                });
+//                // put zero excluding exclude list
+//                $('#gameTable tr td.blank').each(function() {
+//                    if (-1 === $.inArray(this.id, excludeList)) {
+//                        $("#" + this.id).removeClass("blank").addClass("zero");
+//                        RuleFound = 1;
+//                        return false;
+//                    }
+//                });
+//
+//                // if cross is in opposite corner, put 0 in any middle box
+//                excludeList = [];
+//                if (0 == RuleFound) {
+//                    if($("#1-1").hasClass('cross') && $("#3-3").hasClass('cross')) {
+//                        excludeList.push("1-3", "3-1");
+//                    } else if($("#1-3").hasClass('cross') && $("#3-1").hasClass('cross')) {
+//                        excludeList.push("1-1", "3-3");
+//                    }
+//                    $('#gameTable tr td.blank').each(function() {
+//                        if (-1 === $.inArray(this.id, excludeList)) {
+//                            $("#" + this.id).removeClass("blank").addClass("zero");
+//                            RuleFound = 1;
+//                            return false;
+//                        }
+//                    });
+//                }
+//            }
+//        }
+
+        // if nothing found till now, populate randomly
+        if (0 == RuleFound) {
+            $('#gameTable tr td.blank').each(function() {
+                $("#" + this.id).removeClass("blank").addClass("cross");
+                return false;
+            });
+        }
+    }
+
+};
+
 
 
 
@@ -202,14 +303,31 @@ var bindGame = function() {
 
 
 $(document).ready(function() {
-    $('#gameTable tr td.blank').on('click', function() {
-        $("#" + this.id).removeClass("blank").addClass("cross");
+    compRunningX();
+    $('#gameTable tr td').on('click', function() {
+        if($(this).hasClass('blank')) {
+            $("#" + this.id).removeClass("blank").addClass("zero");
 
-        if (checkAllSequence(this.id, 'cross')) {
-            alert('You win');
-            return false;
+            if (checkAllSequence(this.id, 'zero')) {
+                alert('You win');
+                return false;
+            }
+
+            compRunningX();
         }
-
-        compRunningZero();
     });
+
+
+//    $('#gameTable tr td').on('click', function() {
+//        if($(this).hasClass('blank')) {
+//            $("#" + this.id).removeClass("blank").addClass("cross");
+//
+//            if (checkAllSequence(this.id, 'cross')) {
+//                alert('You win');
+//                return false;
+//            }
+//
+//            compRunningZero();
+//        }
+//    });
 });
